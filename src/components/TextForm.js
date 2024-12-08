fce55409a255aa8137ebc628cc0cdabc82cb8d60
rajathacharya <rajath.g.achari11@gmail.com>
2024-12-09 00:51:52 +0530

    fifth commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -63,7 +63,7 @@ export default function TextForm(props) {
         <div className={`container text-${props.textcolor === 'light'? 'dark' : 'light'}`}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnchange} id="myBox"  placeholder="Enter the Text" rows="8"></textarea>
+                <textarea className={`form-control text-${props.textcolor === 'light'? 'dark' : 'light'}`}  value={text} onChange={handleOnchange} style={{backgroundColor: props.mode === 'light' ? 'white' : '#3a3c3f'}} id="myBox"  placeholder="Enter the Text" rows="8"></textarea>
             </div>
             <button className="btn btn-secondary " onClick={handleUpClick} >Convert to Uppercase</button>
             <button className="btn btn-secondary mx-3" onClick={handleLowClick} >Convert to Lowercase</button>
